090db92a58caf5adfc01d26f37753b72441bfe29
nhkim@artience.co.kr <>
2020-04-11 10:14:11 +0900

    0

---
+++ cmax-project-node/homework/object.js
@@ -0,0 +1,91 @@
+let microWave = {
+    name: 'MC-20MW',
+    manufacturer: 'LG', 
+    size : '37cm x 23cm x 30cm',
+    voltage : '220V',
+    power : '1150W',
+    weight: '10kg',
+    color : 'red',
+    work: function(){
+        console.log('It heats food')
+    },
+    price : '200,000 won',
+    volume: '20L',
+}
+let lightStand = {
+    name: 'KS1533F',
+    manufacturer: 'OEM', 
+    size : '37cm x 37cm x 43cm ',
+    voltage : '220V',
+    power : '9W',
+    weight: '4.3kg',
+    color : 'white',
+    work: function(){
+        console.log('It lights room')
+    },
+    price : '40,000 won',
+    volume: '-',
+}
+
+let remoteControl = {
+    name: 'OD-200',
+    manufacturer: 'LG', 
+    size : '3cm x 4.8cm x 19cm ',
+    voltage : '-',
+    power : 'AAA 1.5V',
+    weight: '4.3kg',
+    color : 'white',
+    work: function(){
+        console.log('It turns tv on')
+    },
+    price : '20,000 won',
+    volume: '-',
+}
+
+let car = {
+    name: 'maybach',
+    manufacturer: 'DG', 
+    size : '3.6m x 2.0m x 2m',
+    voltage : '-',
+    power : 'engine V',
+    weight: '1500kg',
+    color : 'blue',
+    work: function(){
+        console.log('It runs stably')
+    },
+    price : '700,000,000 won',
+    volume: '2000cc',
+}
+
+
+let caracterMario = {
+    name: 'Mario',
+    manufacturer: 'JP', 
+    size : '155cm',
+    gender : 'Male',
+    age : '25',
+    weight: '60kg',
+    color : 'red',
+    work: function(){
+        console.log('He works as a plumber ')
+    }
+
+}
+
+let user = {
+    id = "Auo",
+    fullName = 'Full Auo',
+    email = 'auo@gmail.com',
+    country = 'SouthKorea',
+    gender = 'Female',
+    hobby = function(){
+        console.log('She dance like a MJ')
+    }
+}
+
+let thisDinner = {
+    name = '비빔밥',
+    ingredients : '밥, 나물, 고추장',
+    color = 'red',
+    taste = 'hot and sweet',
+}
